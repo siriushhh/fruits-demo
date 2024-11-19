9a434e3c2ba03de405b12316cc12a88c1d0c42e4
sirius <2017276413@qq.com>
2024-11-19 15:01:54 +0800

    添加注释

--- fruits-demo/Features/Home/FruitDetailPage.swift
+++ fruits-demo/Features/Home/FruitDetailPage.swift
@@ -37,9 +37,9 @@ class ApplePage: UIViewController {
 import UIKit
 import SnapKit
 
-class FruitDetailPage: UIViewController {
+class FruitDetailPage: UIViewController {//class FruitDetailPage：继承自 UIViewController，表示这是一个屏幕页面
 
-    var item: CellData?
+    var item: CellData?//用于存储要显示的水果的详细信息，CellData 是一个包含水果数据的结构体
     
     private let titleLabel = UILabel()
     private let imageView = UIImageView()
@@ -47,7 +47,7 @@ class FruitDetailPage: UIViewController {
     
     let startButton = UIButton()
 
-    override func viewDidLoad() {
+    override func viewDidLoad() {//viewDidLoad() 方法：页面加载后会被调用的方法，负责设置 UI 和逻辑
         super.viewDidLoad()
         view.backgroundColor = .white
         
@@ -59,7 +59,7 @@ class FruitDetailPage: UIViewController {
                 view.addSubview(descriptionLabel)
                 view.addSubview(imageView)
         
-        // 设置水果的图片
+        // imageview设置水果的图片：如果 item 有 imageName 属性，则加载对应的图片
                 if let imageName = item?.imageName {
                     imageView.image = UIImage(named: imageName)
                 }
@@ -88,17 +88,20 @@ class FruitDetailPage: UIViewController {
         
         startButton.setTitle("Back", for: .normal)
         startButton.setTitleColor(UIColor(red: 0.5, green: 0.7, blue: 1.0, alpha: 1.0), for: .normal)
-        view.addSubview(startButton)
+        view.addSubview(startButton)//添加按钮到视图
         startButton.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(10)  // 10是与顶部的间距
             make.leading.equalTo(view.safeAreaLayoutGuide).offset(10)  // 10是与左边的间距
         }
-        startButton.addTarget(self, action: #selector(startButtonTap), for: .touchUpInside)
+        startButton.addTarget(self, action: #selector(startButtonTap), for: .touchUpInside)//for: .touchUpInside:事件类型为按钮被按下且手指抬起时触发
         
     }
+    //@objc private func startButtonTap()：返回按钮绑定了点击事件，当点击时，会调用该方法
     @objc private func startButtonTap() {
-        var vc = UIViewController()
+        //var vc = UIViewController()
         navigationController?.popViewController(animated: true)
+        //navigationController 是 UIViewController的一个属性，表示当前视图控制器所在的导航控制器堆栈
+        //popViewController(animated:)用于从导航控制器堆栈中移除当前视图控制器，并返回到堆栈中的上一个视图控制器
     }
 }
 
--- fruits-demo/Features/Home/MainPage.swift
+++ fruits-demo/Features/Home/MainPage.swift
@@ -99,25 +99,27 @@ class MainPage: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     // UITableView DataSource methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return data.count//data.count 是数组 data 中元素的总数，有标签、图片、详细解释共三个元素
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         let cellData = data[indexPath.row]
-        cell.textLabel?.text = cellData.title
+        cell.textLabel?.text = cellData.title//设置单元格（滚动列表）的内容，包括title和image
         cell.imageView?.image = UIImage(named: cellData.imageName)
         return cell
     }
 
-    // UITableView Delegate method
+    // UITableView Delegate method：处理用户在 UITableView 中选择某一行时触发的逻辑
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //indexPath: 表示被选中的行的位置。  row: 表示该行在段中的索引（从 0 开始计数）
         let cellData = data[indexPath.row]
-        
-        let detailVC = FruitDetailPage()
+        //data 是一个数组，存储了每一行的水果数据（CellData 类型）
+        //indexPath.row 提供被点击行的索引，通过索引获取对应的水果数据并存储在 cellData 中
+        let detailVC = FruitDetailPage()//创建并初始化详情页
         detailVC.item = cellData // 传递水果数据
-        navigationController?.pushViewController(detailVC, animated: true)
-        tableView.deselectRow(at: indexPath, animated: true)
+        navigationController?.pushViewController(detailVC, animated: true)//当前表格页面导航到水果详情页面，用户可以看到水果的详细信息
+        tableView.deselectRow(at: indexPath, animated: true)//取消被选中行的高亮状态，取消选中的过程会有一个淡出的动画效果
     }
 
 
